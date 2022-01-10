a3621575e4cfa81b573f17c707c51476e24c3c51
Mikhail Krivonosov <mike_live@mail.ru>
2022-01-10 18:32:33 +0300

    numba for border cases.

--- src/get_border_cases.py
+++ src/get_border_cases.py
@@ -1,13 +1,16 @@
 import numpy as np
+from numba import njit
 
 # This function counts border states for a square.
 # N - index of last item in line (counting starts from 0).
 # So if there are 17 states in line, N will be 16.
 
+@njit
 def get_border_cases(N):
     border_cases = []
-    for i in np.arange(0, N + 1):
+    for i in [0, N]:
         for j in np.arange(0, N + 1):
-            if (i == 0 or i == N or j == 0 or j == N):
-                border_cases.append(i * (N + 1) + j)
+            border_cases.append(i * (N + 1) + j)
+            if i != j:
+                border_cases.append(j * (N + 1) + i)
     return border_cases
