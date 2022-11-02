dbac5de3fc80a5ecde2aa902ad065f6edaedf0a1
Sergei Tikhomirov <sergey.zr.co.uk@gmail.com>
2022-11-02 22:04:25 +0300

    Small edits

--- src/get_prob_matrix.py
+++ src/get_prob_matrix.py
@@ -21,15 +21,15 @@ def get_prob_matrix(N, games_states, return_counts=False):
     frequencies = counts.astype(np.float64)
     frequencies /= np.maximum(1, frequencies.sum(axis = 1)[np.newaxis].T)
     qr = frequencies.copy()
-    frequencies = get_frequencies(N, frequencies)
+    frequencies = get_frequencies(qr, N)
     if return_counts:
         return qr, frequencies, counts
     else:
         return qr, frequencies
 
-def get_frequencies(N, frequencies):
+def get_frequencies(qr, N):
     border = sorted(get_border_cases(N)) # from greatest to smallest
     for i in np.arange(len(border)-1, -1, step = -1):
-        frequencies = np.delete(frequencies, border[i], axis = 0)
-        frequencies = np.delete(frequencies, border[i], axis = 1)
-    return frequencies
+        qr = np.delete(qr, border[i], axis = 0)
+        qr = np.delete(qr, border[i], axis = 1)
+    return qr
--- src/get_prob_matrix_symmetry.py
+++ src/get_prob_matrix_symmetry.py
@@ -26,7 +26,7 @@ def get_prob_matrix_symmetry(N, games_states):
     for i in range (0, frequencies.shape[0]):
         if (sum(frequencies[i]) != 0):
             frequencies[i] /= sum(frequencies[i])
-    qr = frequencies 
+    qr = frequencies.copy()
     border = sorted(get_border_cases(N)) # from greatest to smallest
     for i in np.arange(len(border)-1, -1, step = -1):
         frequencies = np.delete(frequencies, border[i], axis = 0)
