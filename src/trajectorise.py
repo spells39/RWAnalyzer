13219dc640b724e40cf5dd3302d00f13c60d50e9
spells39 <spells228@gmail.com>
2026-06-09 01:37:46 +0300

    fix

--- src/trajectorise.py
+++ src/trajectorise.py
@@ -2,16 +2,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import os
-def trajectorise(game, case, length, index):
-    fig, axes = plt.subplots(1, figsize=(8, 8), dpi=100)   
-    axes.set_yticks(np.arange(1, 16, 1))
-    axes.set_xticks(np.arange(1, 16, 1))
-    axes.set_xlim(0, 16)
-    axes.set_ylim(0, 16)
-    axes.set_yticklabels([])
+
+
+def _configure_field(axes, field_size=16):
+    axes.set_xticks(np.arange(1, field_size, 1))
+    axes.set_yticks(np.arange(1, field_size, 1))
+    axes.set_xlim(0, field_size)
+    axes.set_ylim(0, field_size)
     axes.set_xticklabels([])
-    axes.grid()
+    axes.set_yticklabels([])
+    axes.grid(True)
+    axes.set_aspect("equal")
     axes.invert_yaxis()
+
+
+def save_empty_field(output_path, field_size=16, figsize=(8, 8), dpi=100):
+    fig, axes = plt.subplots(1, figsize=figsize, dpi=dpi)
+    _configure_field(axes, field_size)
+    fig.savefig(
+        output_path,
+        dpi=dpi,
+        bbox_inches="tight",
+        facecolor="white",
+        edgecolor="white",
+    )
+    plt.close(fig)
+
+
+def trajectorise(game, case, length, index):
+    fig, axes = plt.subplots(1, figsize=(8, 8), dpi=100)
+    _configure_field(axes)
     
     files = glob.glob('../Trajectories/' + str(case) + '/' + str(length) + '/temp/*.png', recursive=True)
     for f in files:
@@ -41,4 +61,4 @@ def trajectorise(game, case, length, index):
     plt.savefig('../Trajectories/' + str(case) + '/' + str(length) + '/' +
                      str(case) + '_' + str(length) + '_' + '%02d' % (index,) + '.png', transparent=False, \
                 facecolor='white', edgecolor='white')
-    axes.clear()
+    plt.close(fig)
